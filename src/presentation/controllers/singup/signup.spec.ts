05509e8cd14bfeefdd8d2957d7d52351fb227d9c
demetriusleonardobantim <demetriusleonardobantim98@hotmail.com>
2021-02-17 13:31:45 -0300

    test: ensure SignUpController returns 500 iff ADdAccount throws

--- src/presentation/controllers/singup/signup.spec.ts
+++ src/presentation/controllers/singup/signup.spec.ts
@@ -196,4 +196,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
   });
+  test('Should return 500 if AddAccount throws', () => {
+    const { sut, addAccountStub } = makeSut();
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const httpRequest = {
+      body: {
+        name: 'anyName',
+        email: 'anyEmail@email.com',
+        password: 'anyPassword',
+        passwordConfirmation: 'anyPassword',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
 });
