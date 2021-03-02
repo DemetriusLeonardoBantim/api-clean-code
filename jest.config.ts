fa5e17dfd5a202d5f7a68ed87b1ed42ba5291cb7
demetriusleonardobantim <demetriusleonardobantim98@hotmail.com>
2021-03-02 07:03:50 -0300

    chore: some refactor

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
-};
+}
