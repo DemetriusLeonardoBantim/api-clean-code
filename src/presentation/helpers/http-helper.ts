1e31dc6f71e668d808cb95488c6c408a911a61f1
demetriusleonardobantim <demetriusleonardobantim98@hotmail.com>
2021-03-04 23:23:41 -0300

    refactor: make server error return the error stack

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,19 +1,19 @@
-import { HttpResponse } from '../protocols/http';
-import { ServerError } from '../errors/server-error';
+import { HttpResponse } from '../protocols/http'
+import { ServerError } from '../errors/server-error'
 
 export const badRequest = (error: Error): HttpResponse => {
   return {
     statusCode: 400,
     body: error,
-  };
-};
+  }
+}
 
-export const serverError = (): HttpResponse => ({
+export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
-  body: new ServerError(),
-});
+  body: new ServerError(error.stack),
+})
 
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data,
-});
+})
